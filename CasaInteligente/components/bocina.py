11632698a8a2d1ce8aae8ac46608330dbf812f7c
miguel17546mals <miguel17546mals@gmail.com>
2020-11-10 15:59:16 -0600

    Se agregan comentarios

--- CasaInteligente/components/bocina.py
+++ CasaInteligente/components/bocina.py
@@ -6,15 +6,31 @@ def play_sound():
     while pygame.mixer.music.get_busy():
         continue
 
+"""
+Esta clase es para utilizar el jack 3.5 mm para reproducir sonido
+"""
 class Bocina(object):
     def __init__(self, path, name):
+        """
+        Constructor del objeto
+
+        Args:
+            path (str): Path donde se encuentra el archivo de sonido que se va a reproducir en la alarma
+            name (str): Nombre del dispositivo
+        """        
         self.name = name
         self.path = path
         pygame.mixer.init()
         pygame.mixer.music.load(path)
 
     def on(self):
+        """
+        Crea un Thread para reproducir el archivo
+        """        
         Thread(target=play_sound).start()
 
     def off(self):
+        """
+        Detiene la reproducción
+        """        
         pygame.mixer.music.stop()
